b835a3830dc5fc1b6c8c674575d7addf25fb612b
Lynart <vince.lee.lien@gmail.com>
2013-03-27 15:01:42 -0400

    Initial workings of adding in a template pattern

---
+++ App_Code/PatternClasses/BMITemplate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BMITemplate
+/// </summary>
+public class BMITemplate:CalculationTemplate
+{
+	public BMITemplate()
+	{
+	}
+    public Double calculate(Double weight, Double height)
+    {
+        return weight / (Math.Pow(height / 100, 2));
+    }
+}
---
+++ App_Code/PatternClasses/CalculationTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Used to simplifiy calculatons involving weight by adding the conversion functions here
+/// </summary>
+public abstract class CalculationTemplate
+{
+	public CalculationTemplate()
+	{
+	}
+
+    public Double metricCalculate(Double weight, Double height)
+    {
+        return calculate(weight, height);
+    }
+
+    public Double imperialCalculate(Double weight, Double foot, Double inch)
+    {
+        return calculate(weight, foot * 30.48 + inch * 2.54);
+    }
+
+    public abstract Double calculate(Double weight, Double height);
+}
